0c9df1155da8586d26b8d900a133f9bfbbb25875
Avinash Balaso Jagtap <avinashjgtp10@gmail.com>
2020-01-22 09:34:44 +0530

    change

--- Login.js
+++ Login.js
@@ -1,14 +1,15 @@
-var attempt = 3; // Variable to count number of attempts.
+ // Variable to count number of attempts.
 // Below function Executes on click of login button.
 function validate()
 {
+	var attempt = 0;
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
-		if(username == "" || password == "")
-		{
-			document.getElementById("submit").disabled=true;
-			return false;
-		}
+	if(username === "" || password === "")
+	{
+		document.getElementById("submit").disabled=true;
+		return false;
+	}
 		else
 		{
 			if ( username == "root" && password == "abc123")
@@ -19,13 +20,14 @@ function validate()
 			}
 			else{
 				attempt --;// Decrementing by one.
-				alert("Invalid Login !!! "+"\n You have left "+attempt+" attempt;");
+				
 				// Disabling fields after 3 attempts.
 					if( attempt == 0)
 					{
 						document.getElementById("username").disabled = true;
 						document.getElementById("password").disabled = true;
 						document.getElementById("submit").disabled = true;
+						alert("Invalid Login !!! "+"\n You have left "+attempt+" attempt;");
 						return false;
 					}
 				}
